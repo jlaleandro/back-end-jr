405fa297cb82254d004852292182c4883c683481
jlaleandro <jlaleandro@gmail.com>
2025-09-06 15:30:43 -0300

    Projeto back-end-jr.

---
+++ src/main/java/com/jla/back_end_jr/BackEndJrApplication.java
@@ -0,0 +1,13 @@
+package com.jla.back_end_jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndJrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jla/back_end_jr/BackEndJrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jla.back_end_jr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
